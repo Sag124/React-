5b943afc71198783f6753ff848d9b67223e26279
Raghavendra sagar <sagar05091997@gmail.com>
2018-09-28 18:16:05 +0530

    Modified index.js file

--- src/index.js
+++ src/index.js
@@ -21,17 +21,17 @@ const user = {
 
 const element = (
 	<h1>
-	Hello, {getGreeting(user)}!
+	Hello, {formatName(user)}!
 	</h1>
 	);
 
-function getGreeting(user) {
-	if (user) {
-		return <h1>{formatName(user)}</h1>	
-	}
-		return <h1>Hello stranger </h1>
+// function getGreeting(user) {
+// 	if (user) {
+// 		return <h1>{formatName(user)}</h1>	
+// 	}
+// 		return <h1>Hello stranger </h1>
 
-}
+// }
 
 
 ReactDOM.render(
